8a561f5ca4d8672e535ea5895045f5f27b76cf39
Dimas Agil S. W. <project.dasw@gmail.com>
2025-07-06 17:15:54 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ SESSION_CONFIGS = [
         'name': 'digital_payment',
         'display_name': "Digital Payment Experiment",
         'num_demo_participants': 1,
-        'app_sequence': ['welcome_page', 'digital_payment'],
-    },
+        'app_sequence': ['welcome_instructions', 'digital_payment'],
+    }
 ]
 
 
